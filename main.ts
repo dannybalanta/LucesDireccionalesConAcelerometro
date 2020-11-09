4797a3612fb0d768d5784690e9cef8c377acd36e
dannybalanta <42857316+dannybalanta@users.noreply.github.com>
2020-11-09 15:46:01 -0500

    minimal changes

--- main.ts
+++ main.ts
@@ -1,1 +1,85 @@
-
+input.onGesture(Gesture.Shake, function () {
+    for (let index = 0; index < 4; index++) {
+        basic.pause(200)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . # # # .
+            # # # # #
+            `)
+        basic.pause(200)
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # # # # #
+            . . # . .
+            . . # . .
+            `)
+    }
+})
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.AB)) {
+        for (let index = 0; index < 4; index++) {
+            basic.pause(200)
+            basic.showLeds(`
+                . . . . .
+                . # # # .
+                . # # # .
+                . # # # .
+                . . . . .
+                `)
+            basic.pause(200)
+            basic.showLeds(`
+                . # # # .
+                # # # # #
+                # # # # #
+                # # # # #
+                . # # # .
+                `)
+            basic.pause(200)
+        }
+    } else if (input.buttonIsPressed(Button.A)) {
+        for (let index = 0; index < 4; index++) {
+            basic.pause(200)
+            basic.showLeds(`
+                . . . . #
+                . . . # #
+                . . # # #
+                . . . # #
+                . . . . #
+                `)
+            basic.pause(200)
+            basic.showLeds(`
+                . . # . .
+                . # # . .
+                # # # # #
+                . # # . .
+                . . # . .
+                `)
+            basic.pause(200)
+        }
+    } else if (input.buttonIsPressed(Button.B)) {
+        for (let index = 0; index < 4; index++) {
+            basic.pause(200)
+            basic.showLeds(`
+                # . . . .
+                # # . . .
+                # # # . .
+                # # . . .
+                # . . . .
+                `)
+            basic.pause(200)
+            basic.showLeds(`
+                . . # . .
+                . . # # .
+                # # # # #
+                . . # # .
+                . . # . .
+                `)
+            basic.pause(200)
+        }
+    } else {
+        basic.clearScreen()
+    }
+})
